b7f92c803b543a920b0d274e1a42c30a1da6c6d0
JunmanChoi <junman9595@gmail.com>
2024-12-03 04:20:04 +0900

    feat: manage 그룹 레이아웃에 툴바 구성 컴포넌트 적용

--- app/(manage)/layout.tsx
+++ app/(manage)/layout.tsx
@@ -1,13 +1,11 @@
 import ExpandButton from '@/components/side-bar/ExpandButton';
+import PageTitle from '@/components/tool-bar/PageTitle';
+import UserInfo from '@/components/tool-bar/UserInfo';
 import { Box, Flex, Text } from '@radix-ui/themes';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Flex className="absolute h-screen w-screen overflow-hidden">
       <Box
@@ -22,12 +20,7 @@ export default function Layout({
               align="center"
               className="h-full text-white font-semibold"
             >
-              <Image
-                src="/icons/task.svg"
-                alt="task"
-                height={20}
-                width={20}
-              />
+              <Image src="/icons/task.svg" alt="task" height={20} width={20} />
               <Text aria-label="tasks">tasks</Text>
             </Flex>
           </Link>
@@ -39,20 +32,18 @@ export default function Layout({
               align="center"
               className="h-full text-white font-semibold"
             >
-              <Image
-                src="/icons/user.svg"
-                alt="user"
-                height={20}
-                width={20}
-              />
+              <Image src="/icons/user.svg" alt="user" height={20} width={20} />
               <Text aria-label="users">users</Text>
             </Flex>
           </Link>
         </button>
       </Box>
-      <Flex className="relative grow h-full" direction="column">
-        <Box className="flex-shrink-0 h-10 bg-amber-400">this</Box>
-        <Box className="relative h-full border-box m-5 bg-amber-500">
+      <Flex className="relative grow h-full p-5" gap="4" direction="column">
+        <Flex justify="between" className="flex-shrink-0 h-10 bg-amber-400">
+          <PageTitle />
+          <UserInfo />
+        </Flex>
+        <Box className="relative h-full border-box bg-amber-500">
           {children}
         </Box>
       </Flex>
