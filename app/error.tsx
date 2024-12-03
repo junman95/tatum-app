2fd6f99c78c12d245bbb832601f2ea49dad204cf
JunmanChoi <junman9595@gmail.com>
2024-12-04 00:27:57 +0900

    feat: 에러 타입 분리

--- app/error.tsx
+++ app/error.tsx
@@ -3,21 +3,44 @@ import { Flex, Heading } from '@radix-ui/themes';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
+enum CommonError {
+  Default = 'Default',
+  UserNotFound = 'User not found',
+}
+
+const getErrorMessage = (error: Error) => {
+  const errorMessages = {
+    [CommonError.Default]: {
+      title: `에러 발생: ${error.message}`,
+      message: '2초 뒤 로그인 화면으로 이동합니다.',
+    },
+    [CommonError.UserNotFound]: {
+      title: '가입되지 않은 이메일 입니다. 확인해 주세요.',
+      message: '2초 뒤 로그인 화면으로 이동합니다.',
+    },
+  };
+
+  return (
+    errorMessages[error.message as CommonError] ||
+    errorMessages[CommonError.Default]
+  );
+};
+
 export default function Error({
   error,
 }: {
   error: Error & { digest?: string };
 }) {
   const router = useRouter();
+  const { title, message } = getErrorMessage(error);
   useEffect(() => {
-    if (error.message === 'User not found') {
+    if (error.message === CommonError.UserNotFound) {
       new Promise(() => {
         setTimeout(() => {
           router.push('/login');
         }, 2000);
       });
     }
-    console.error(error);
   }, [error, router]);
 
   return (
@@ -28,8 +51,8 @@ export default function Error({
       className="h-svh"
       gap="6"
     >
-      <Heading size="7">Error: {error.message}</Heading>
-      <Heading size="5">2초 뒤 로그인 화면으로 이동합니다.</Heading>
+      <Heading size="7">{title}</Heading>
+      <Heading size="5">{message}</Heading>
     </Flex>
   );
 }
