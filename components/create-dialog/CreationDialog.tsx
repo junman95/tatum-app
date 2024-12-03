b6f163d837b48fd1840ae136f54db4da014c1314
JunmanChoi <junman9595@gmail.com>
2024-12-04 04:46:29 +0900

    feat: 데이터 생성 폼을 담는 다이얼로그 구현

---
+++ components/create-dialog/CreationDialog.tsx
@@ -0,0 +1,18 @@
+import { Button, Dialog } from '@radix-ui/themes';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const CreationDialog = ({ children }: Props) => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button>click</Button>
+      </Dialog.Trigger>
+      <Dialog.Content>{children}</Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default CreationDialog;
---
+++ stories/components/create-dialog/CreationDialog.stories.tsx
@@ -0,0 +1,29 @@
+import CreationDialog from '@/components/create-dialog/CreationDialog';
+import TaskForm from '@/components/create-dialog/TaskForm';
+import { Button, Dialog } from '@radix-ui/themes';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/CreateDialog/CreationDialog',
+  component: CreationDialog,
+  decorators: (Story) => (
+    <div className="flex justify-center items-center h-screen">
+      <Story />
+    </div>
+  ),
+} satisfies Meta<typeof CreationDialog>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const TaskFormDialog = {
+  args: {
+    children: (
+      <TaskForm>
+        <Dialog.Close>
+          <Button>close</Button>
+        </Dialog.Close>
+      </TaskForm>
+    ),
+  },
+} satisfies Story;
